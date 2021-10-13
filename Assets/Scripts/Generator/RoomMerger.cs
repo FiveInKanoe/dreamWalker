c4db0b9d21925036af115ba8869e90dc99308ce0
Vitalii Povkh <fehold2000@gmail.com>
2021-10-13 20:55:34 +0300

    Cluster generator has been improved. Structures have been implemented and added

--- Assets/Scripts/Generator/RoomMerger.cs
+++ Assets/Scripts/Generator/RoomMerger.cs
@@ -7,35 +7,48 @@ public class RoomMerger : MonoBehaviour
 {
 
     [SerializeField] private ClusterGenerator clustGen;
+    [SerializeField] private List<GameObject> structures;
+    [SerializeField] private float probability;
 
     private RoomCell[,] cellMatrix;
+    private int roomWidth;
+    private int roomHeight;
 
+    private List<LayerInfo> layersInf;
+    private List<List<BoundsInt>> placesForStructures;
 
     void Start()
     {
+        placesForStructures = new List<List<BoundsInt>>();
+        layersInf = new List<LayerInfo>();
+
         cellMatrix = clustGen.GetCuttedMapMatrix();
+
+        placesForStructures.Add(new List<BoundsInt>());
         MakeMap();
-    }
-
-
-    void FixedUpdate()
-    {
-        
+        for (int i = 0; i < structures.Count; i++)
+        {
+            placesForStructures.Add(new List<BoundsInt>());
+            CreateStructures(structures[i].GetComponent<RoomView>().Layers[0], i + 1);
+        }   
     }
 
     private void MakeMap()
     {
         List<GameObject> layers = new List<GameObject>()
         {
-            { new GameObject("Grass") },
-            { new GameObject("Road") },
-            { new GameObject("Border") }
+            new GameObject("Grass"),
+            new GameObject("Road"),
+            new GameObject("Border"),
+            new GameObject("Decoration")
+
         };
 
-        List<LayerInfo> layersInf = new List<LayerInfo>();
-
+        int zPos = layers.Count;
         foreach (GameObject layer in layers)
         {
+            layer.transform.position = new Vector3(0, 0, zPos--);
+
             layer.transform.SetParent(transform);
             Tilemap tmap = layer.AddComponent(typeof(Tilemap)) as Tilemap;
             layer.AddComponent(typeof(TilemapRenderer));
@@ -56,8 +69,8 @@ public class RoomMerger : MonoBehaviour
                 {
                     RoomView roomInfo = cell.Room.GetComponent<RoomView>();
 
-                    int roomWidth = roomInfo.Layers[0].cellBounds.xMax;
-                    int roomHeight = roomInfo.Layers[0].cellBounds.yMax;
+                    roomWidth = roomInfo.Layers[0].cellBounds.xMax;
+                    roomHeight = roomInfo.Layers[0].cellBounds.yMax;
 
                     xRoomOffset = roomWidth * j;
                     yRoomOffset = roomHeight * i;
@@ -65,8 +78,13 @@ public class RoomMerger : MonoBehaviour
                     List<TileBase[]> roomLayers = new List<TileBase[]>();
                     foreach (Tilemap tmap in roomInfo.Layers)
                     {
-                        BoundsInt squareToGetFrom = new BoundsInt(new Vector3Int(0, 0, 0), new Vector3Int(roomWidth, roomHeight, 1)); //баг где-то тут      
+                        BoundsInt squareToGetFrom = new BoundsInt
+                            (
+                            new Vector3Int(0, 0, 0),
+                            new Vector3Int(roomWidth, roomHeight, 1)
+                            );
                         roomLayers.Add(tmap.GetTilesBlock(squareToGetFrom));
+
                     }
 
                     int tileCounter = 0;
@@ -78,19 +96,124 @@ public class RoomMerger : MonoBehaviour
                             {
                                 layersInf[k].Tilemap.SetTile
                                     (
-                                    new Vector3Int(xRoomOffset + x, yRoomOffset + y, layersInf.Count - k),
+                                    new Vector3Int(xRoomOffset + x, yRoomOffset + y, 0),
                                     roomLayers[k][tileCounter]
                                     );
                             }
                             tileCounter++;
                         }
                     }
-                    
+
+                    if (cell == clustGen.StartCell || cell == clustGen.ExitCell)
+                    {
+                        placesForStructures[0].Add(new BoundsInt
+                            (
+                            new Vector3Int(xRoomOffset, yRoomOffset, 0),
+                            new Vector3Int(roomWidth - 1, roomHeight - 1, 1)
+                            ));
+                    }
                 }
             }
         }
     }
 
+    private void CreateStructures(Tilemap structDecouration, int structureIdx)
+    {
+        //Список, хранящий области, подходящие под размещение структуры
+        List<BoundsInt> placesList = new List<BoundsInt>();
+        //Получаем слой травы уже сгенерированной карты
+        Tilemap grassLayer = layersInf[0].Tilemap;
+
+        //Получаем общаие размеры слоя травы (то есть, всей карты)
+        int mapWidth = grassLayer.cellBounds.xMax;
+        int mapHeight = grassLayer.cellBounds.yMax;
+        Debug.Log($"width {mapWidth}  height{mapHeight}");
+        //Получаем массив всех тайлов слоя травы
+        TileBase[] tiles = grassLayer.GetTilesBlock(grassLayer.cellBounds);
+
+        //Получаем размеры структуры
+        int structWidth = structDecouration.cellBounds.xMax;
+        int structHeight = structDecouration.cellBounds.yMax;
+        Debug.Log($"structwidth {structWidth}  structheight{structHeight}");
+        //Пееребираем все тайлы на карте
+        for (int mapI = structHeight; mapI < mapHeight; mapI += structHeight + 1)
+        {
+            for (int mapJ = structWidth; mapJ < mapWidth; mapJ += structWidth + 1)
+            {
+                //Флаг, для выхода из цикла, если площадь уже не будет подходящей
+                bool notMatchingSpace = false;
+                //Выделим на рассмотрение конкретный блок тайлов с основной карты
+                BoundsInt currentArea  = new BoundsInt
+                    (
+                    new Vector3Int(mapJ - structWidth, mapI - structHeight, 0),
+                    new Vector3Int(structWidth, structHeight, 1)
+                    );
+                for (int i = mapI - structHeight; i < mapI; i++)
+                {
+                    for (int j = mapJ - structWidth; j < mapJ; j++)
+                    {
+
+                        notMatchingSpace = !grassLayer.HasTile(new Vector3Int(j, i, 0))
+                            || !grassLayer.HasTile(new Vector3Int(j + 1, i, 0))
+                            || !grassLayer.HasTile(new Vector3Int(j - 1, i, 0))
+                            || !grassLayer.HasTile(new Vector3Int(j, i + 1, 0))
+                            || !grassLayer.HasTile(new Vector3Int(j, i - 1, 0))
+                            || !grassLayer.HasTile(new Vector3Int(j + 1, i + 1, 0))
+                            || !grassLayer.HasTile(new Vector3Int(j - 1, i - 1, 0))
+                            || !grassLayer.HasTile(new Vector3Int(j + 1, i - 1, 0))
+                            || !grassLayer.HasTile(new Vector3Int(j - 1, i + 1, 0));
+                        
+                        foreach (List<BoundsInt> areasOfstructure in placesForStructures)
+                        {
+                            foreach (BoundsInt area in areasOfstructure)
+                            {
+                                if (j >= area.xMin && j <= area.xMax && i >= area.yMin && i <= area.yMax)
+                                {
+                                    notMatchingSpace = true;
+                                    break;
+                                }
+                            }
+                            if (notMatchingSpace) break;
+                        }
+                        if (notMatchingSpace) break;
+                    }
+                    if (notMatchingSpace) break;
+                }
+                //Если место оказалось подходящим - добавить в список подходящих мест
+                if (!notMatchingSpace)
+                {
+                    placesList.Add(currentArea);
+                }
+            }
+        }
+
+        foreach (BoundsInt freeArea in placesList)
+        {
+            if (Random.Range(0f, 1f) <= probability)
+            {
+                bool res = placesForStructures[structureIdx].Exists(delegate (BoundsInt place)
+                {
+                    bool nPlace = freeArea.xMin - 1 == place.xMax
+                    || freeArea.xMax + 1 == place.xMin
+                    || freeArea.yMin - 1 == place.yMax
+                    || freeArea.yMax + 1 == place.yMin;
+                    return nPlace; 
+                });
+                if (!res)
+                {
+                    placesForStructures[structureIdx].Add(freeArea);
+                }         
+            }  
+        }
+
+        
+
+        foreach (BoundsInt freeArea in placesForStructures[structureIdx])
+        {
+            layersInf[3].Tilemap.SetTilesBlock(freeArea, structDecouration.GetTilesBlock(structDecouration.cellBounds));
+        }
+
+    }
 
 
     private class LayerInfo
